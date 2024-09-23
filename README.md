064195a1d82120808d633c42e9c0277f2ec16ca4
airvt1x <airat3552@gmail.com>
2024-09-24 01:56:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Программирование корпоративных систем
+
+## 3 работа
+
+![image](https://github.com/user-attachments/assets/b1a4e358-3bac-4a60-b772-ede9470fd6f8)
+
+![image](https://github.com/user-attachments/assets/300a38bc-9ad0-409b-bf35-51ecd9442bee)
---
+++ lib/data/items.dart
@@ -0,0 +1,28 @@
+import 'package:project_name/models/item_class.dart';
+
+final List<ItemClass> items = [
+  ItemClass(
+    1,
+    'https://img.razrisyika.ru/kart/127/507521-sotovyy-telefon-6.jpg',
+    5990,
+    'Apple Nokia Samsung',
+    'Сразу несколько, представь.',
+    'Продается сразу несколько новеньких телефонов только вместе.',
+  ),
+  ItemClass(
+    2,
+    'https://burobiz-a.akamaihd.net/uploads/images/104287/large_remont-i-prodazha-bu-mobilnyx-telefonov-xoroshee-nachalo-dlya-perspektivnogo-biznesa-www-ural-org.jpg',
+    6690,
+    'Nokia',
+    'Неворованный синий нокиа с фото',
+    'Отличный телефон, держит заряд месяц',
+  ),
+  ItemClass(
+    3,
+    'https://cdn1.ozone.ru/s3/multimedia-b/6014927879.jpg',
+    7990,
+    'Texet',
+    'Неубиваемый телефон, пережил ядерную войну',
+    'Этим телефоном люди впервые научились добывать огонь, охотиться на мамонтов, это он нагрузил Грузию, затонировал черное море, а мертвое убил, успокоил тихий океан и покрасил красную площадь',
+  )
+];
